cc68d9e6ae5bbe1d89526e3c0ac8653ba4a4f451
GinBaby <jdog74@gmail.com>
2017-01-18 01:30:12 -0600

    update maybe example

--- maybe.hs
+++ maybe.hs
@@ -1,17 +1,15 @@
-import Data.Monoid
-
 data Person = Person {
       name   :: String
     , age    :: Int
     , pl     :: String
    } deriving (Eq, Show)
 
-noEmpty :: String -> Maybe String
-noEmpty ""  = Nothing
-noEmpty str = Just str
+nonEmpty :: String -> Maybe String
+nonEmpty ""  = Nothing
+nonEmpty str = Just str
 
-noNegative :: Int -> Maybe Int
-noNegative n | n >= 0 = Just n
+nonNegative :: Int -> Maybe Int
+nonNegative n | n >= 0 = Just n
              | otherwise = Nothing
 
 plCheck :: Person -> Maybe Person
@@ -27,13 +25,13 @@ mkPerson :: String
             -> String
             -> Maybe Person
 mkPerson name' age' pl' =
-  case noEmpty name' of
+  case nonEmpty name' of
    Nothing -> Nothing
    Just namae ->
-     case noNegative age' of
+     case nonNegative age' of
       Nothing -> Nothing
       Just aged ->
-        case noEmpty pl' of
+        case nonEmpty pl' of
           Nothing -> Nothing
           Just langy ->
               plCheck (Person namae aged langy)
@@ -42,22 +40,82 @@ mkPerson name' age' pl' =
 
 mkPerson' :: String -> Int -> String -> Maybe Person
 mkPerson' name' age' pl' = do
-  namae <- noEmpty name'
-  aged <- noNegative age'
-  langy <- noEmpty pl'
+  namae <- nonEmpty name'
+  aged <- nonNegative age'
+  langy <- nonEmpty pl'
   plCheck (Person namae aged langy)
+
+plChk :: Person -> Either String Person
+plChk c =
+  let p = pl c
+      n = name c
+  in if n == "Simon" && (p /= "Haskell")
+     then Left "All Simons write Haskell."
+     else Right c
+
+-- using Either instead of Maybe gives a better demonstration
+-- of where it's failing and how it doesn't "see" any later error 
+-- once it's already hit a Left case.
+mkPer :: String
+            -> Int
+            -> String
+            -> Either String Person
+mkPer name' age' pl' =
+  case nonEmpty name' of
+   Nothing -> Left "Empty name value."
+   -- if name is empty, it will tell us that
+   -- if it's not empty, it returns Just name and goes on to
+   -- the next check
+   Just named ->
+     case nonNegative age' of
+      Nothing -> Left "Negative age."
+      -- if a negative number, we get Nothing; otherwise, go on
+      Just aged ->
+        case nonEmpty pl' of
+          Nothing -> Left "Empty prog lang."
+          -- and again check for empty strings
+          Just lang ->
+              plChk (Person named aged lang)
 
 -- monad context even though there's no IO here, 
 -- because later computations depend on the result of earlier ones
 
-mkPerson "Simon" 45 "Scala"
-mkPerson "Chris" (-30) "Scala"
+-- mkPer "Simon" (-3) ""
+-- Left "Negative age."
 
-another version with applicative and bind
-mkPer :: String -> Int -> String -> Maybe Person
-mkPer name' age' pl' = 
-  Person <$> (nonEmpty name') 
-         <*> (nonNegative age') 
-         <*> (nonEmpty pl') 
-  >>= plCheck
+per :: String -> Int -> String -> Maybe Person
+per name' age' pl' = Person <$> (nonEmpty name') 
+                            <*> (nonNegative age') 
+                            <*> (nonEmpty pl')
 
+-- desugaring `do` with applicative and bind
+mkPer' :: String -> Int -> String -> Maybe Person
+mkPer' name' age' pl' = 
+  -- Person <$> (nonEmpty name') 
+  --        <*> (nonNegative age') 
+  --        <*> (nonEmpty pl') 
+  per name' age' pl' >>= plCheck
+
+
+noEmpty :: String -> Either String String
+noEmpty ""  = Left "Empty string."
+noEmpty str = Right str
+
+noNegative :: Int -> Either String Int
+noNegative n | n >= 0 = Right n
+              | otherwise = Left "Negative age."
+
+-- plChk :: Person -> Either String Person
+-- plChk c =
+--   let p = pl c
+--       n = name c
+--   in if n == "Simon" && (p /= "Haskell")
+--      then Left "All Simons write Haskell."
+--      else Right c
+
+mkP :: String -> Int -> String -> Either String Person
+mkP name' age' pl' = 
+  Person <$> (noEmpty name') 
+         <*> (noNegative age') 
+         <*> (noEmpty pl') 
+  >>= plChk
